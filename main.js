aaa1b033f782e6ef79f1a275b31e0c7b9c20f0d9
haseebnaeem94 <162363763+haseebnaeem94@users.noreply.github.com>
2024-03-23 14:03:51 +0500

    number game
    This is a CLI based number guessing game

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Please guess the number",
+    }
+]);
+if (answers.randomNumber == answers.userNumber) {
+    console.log("congralutions you guess the correct number");
+}
+else {
+    console.log("you guess wrong number please guess the number again");
+}
